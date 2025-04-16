aa8f80205351d47f7bd2ca1713a15c7f733abecf
Ryan SH <ryanhilario5@hotmail.com>
2025-04-16 19:55:20 -0300

    criando: função de scroll smooth para os links internos

--- script.js
+++ script.js
@@ -39,3 +39,32 @@ function initAccordionFaq() {
   }
 }
 initAccordionFaq();
+
+function initSmoothScroll() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  if (linksInternos.length) {
+    function scrollToSection(event) {
+      event.preventDefault();
+      const href = this.getAttribute("href");
+      const section = document.querySelector(href);
+      const sectionTop = section.offsetTop;
+
+      window.scrollTo({
+        top: sectionTop,
+        behavior: "smooth",
+      });
+
+      //   forma alternativa
+      //   section.scrollIntoView({
+      //     behavior: "smooth",
+      //     block: "start",
+      //   });
+    }
+
+    linksInternos.forEach((link) => {
+      link.addEventListener("click", scrollToSection);
+    });
+  }
+}
+initSmoothScroll();
