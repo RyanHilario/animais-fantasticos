68ffa1aac1524ab6e1e520e7abc5c332d623d1f7
Ryan SH <ryanhilario5@hotmail.com>
2025-04-16 19:27:22 -0300

    criando: accordion list no FAQ

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if (tabMenu.length && tabContent.length) {
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove("ativo");
+  if (tabMenu.length && tabContent.length) {
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add("ativo");
+    activeTab(0);
   }
-
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener("click", () => {
-      activeTab(index);
-    });
-  });
-  activeTab(0);
 }
+initTabNav();
+
+function initAccordionFaq() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const ativoClass = "ativo";
+
+  if (accordionList.length) {
+    accordionList[0].classList.add(ativoClass);
+    accordionList[0].nextElementSibling.classList.add(ativoClass);
+
+    function activeAccordion() {
+      this.classList.toggle(ativoClass);
+      this.nextElementSibling.classList.toggle(ativoClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+initAccordionFaq();
