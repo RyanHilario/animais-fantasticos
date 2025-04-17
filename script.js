f8f5807f811380f98b998a1a861fa8530ceaf5da
Ryan SH <ryanhilario5@hotmail.com>
2025-04-16 23:33:14 -0300

    criando: função de animação das seções ao usar o scroll

--- script.js
+++ script.js
@@ -68,3 +68,26 @@ function initSmoothScroll() {
   }
 }
 initSmoothScroll();
+
+const sections = document.querySelectorAll(".js-scroll");
+const windowMetade = window.innerHeight * 0.6;
+
+function initScrollAnimation() {
+  if (sections.length) {
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+initScrollAnimation();
