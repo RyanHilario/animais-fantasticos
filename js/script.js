2a72c60c3c92a3c6ab7d9c91f925875da786860a
Ryan SH <ryanhilario5@hotmail.com>
2025-04-29 23:35:08 -0300

    criando e modificando: modulos JS das funções / atributos das tags HTML

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initSmoothScroll from "./module/scroll-suave.js";
+import initScrollAnimation from "./module/scroll-animacao.js";
+import initAccordionFaq from "./module/accordion.js";
+import initTabNav from "./module/tabnav.js";
+
+initSmoothScroll();
+initScrollAnimation();
+initAccordionFaq();
+initTabNav();
