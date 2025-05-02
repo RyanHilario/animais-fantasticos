75ae3948ac3e87de896a91bff9531f01b4040786
Ryan SH <ryanhilario5@hotmail.com>
2025-05-01 22:05:38 -0300

    criando: modal para formulário de login

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initSmoothScroll from "./module/scroll-suave.js";
 import initScrollAnimation from "./module/scroll-animacao.js";
 import initAccordionFaq from "./module/accordion.js";
 import initTabNav from "./module/tabnav.js";
+import initModal from "./module/modal.js";
 
 initSmoothScroll();
 initScrollAnimation();
 initAccordionFaq();
 initTabNav();
+initModal();
